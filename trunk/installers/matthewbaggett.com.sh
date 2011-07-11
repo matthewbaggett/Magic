bd730cb32e986d8700478413c9239c1a80381300
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-07-11 17:10:30 +0000

    git-svn-id: http://svn.turbocrms.com/magic@10 031e0832-c032-4354-a283-d89f8bcd3735

--- trunk/installers/matthewbaggett.com.sh
+++ trunk/installers/matthewbaggett.com.sh
@@ -1,3 +1,4 @@
-./core.sh
-svn co http://svn.turbocrms.com/magicapps/matthewbaggett.com/trunk/ ../application/MatthewBaggett.com
+./installers/core.sh
+svn co http://svn.turbocrms.com/magicapps/exception/trunk/ ./application/Exception
+svn co http://svn.turbocrms.com/magicapps/matthewbaggett.com/trunk/ ./application/MatthewBaggett.com
 
