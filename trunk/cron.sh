8ba3a9d574374a498d088b429306fd8679ab2962
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-07-12 13:51:31 +0000

    M    cron.sh
    git-svn-id: http://svn.turbocrms.com/magic@21 031e0832-c032-4354-a283-d89f8bcd3735

--- trunk/cron.sh
+++ trunk/cron.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-/usr/bin/php ./scripts/cron.php
-#/usr/local/bin/php-5.3 ./scripts/cron.php
+MY_PATH="`dirname \"$0\"`"
+echo "$MY_PATH"
+#/usr/bin/php ./scripts/cron.php
+/usr/local/bin/php-5.3 $MY_PATH/scripts/cron.php
