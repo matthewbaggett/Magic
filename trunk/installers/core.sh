4ea2bbbcdac22e95c97e999982951f99f309fddf
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-07-15 09:43:21 +0000

    git-svn-id: http://svn.turbocrms.com/magic@22 031e0832-c032-4354-a283-d89f8bcd3735

--- trunk/installers/core.sh
+++ trunk/installers/core.sh
@@ -2,3 +2,5 @@ svn co http://svn.turbocrms.com/magic/trunk ./
 svn co http://svn.turbocrms.com/magicplugins/cms/trunk/ ./plugins/cms
 svn co http://svn.turbocrms.com/magicplugins/gallery/trunk/ ./plugins/gallery
 svn co http://svn.turbocrms.com/magicplugins/pages/trunk/ ./plugins/pages
+svn co http://svn.turbocrms.com/magicplugins/cdn/trunk/ ./plugins/cdn
+svn co http://svn.turbocrms.com/magicplugins/googlechartapi/trunk/ ./plugins/googlechartapi
