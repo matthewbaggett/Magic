3f414d5f083a982d08f07decaf2e327c8b90e652
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-07-16 02:01:12 +0000

    Adding core installer
    git-svn-id: http://svn.turbocrms.com/magic@25 031e0832-c032-4354-a283-d89f8bcd3735

---
+++ trunk/installers/turbocore.sh
@@ -0,0 +1,4 @@
+./installers/core.sh
+svn co http://svn.turbocrms.com/magicapps/exception/trunk/ ./application/Exception
+svn co http://svn.turbocrms.com/magicapps/core/trunk/ ./application/TurboCore
+
