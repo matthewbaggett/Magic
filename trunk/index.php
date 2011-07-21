b5ee9fab6296e664c9ec00498746fd4252969af3
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-07-21 12:44:52 +0000

    Update to support update.lock
    git-svn-id: http://svn.turbocrms.com/magic@56 031e0832-c032-4354-a283-d89f8bcd3735

--- trunk/index.php
+++ trunk/index.php
@@ -1,4 +1,7 @@
 <?php 
+	if(file_exists("update.lock")){
+		die("Hang on a tick! Site down for maintainance (an update is being deployed!)");
+	}
 	require_once("core/MagicCore.php");
 	$config = MagicApplicationConfiguration::LoadByHost();
 	$app = Application::Factory($config);
