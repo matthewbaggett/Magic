e8b777f3533a91db0e142dc64df032daff42d6c9
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-07-26 15:38:18 +0000

    Adding Wolfram key
    git-svn-id: http://svn.turbocrms.com/magic@67 031e0832-c032-4354-a283-d89f8bcd3735

--- trunk/data/003.third-party.php
+++ trunk/data/003.third-party.php
@@ -1,5 +1,5 @@
 <?php
-if (SettingSearcher::Factory()->search_by_system_name("EMAIL_SMTP_SSL")->count() == 0) {
+if (SettingSearcher::Factory()->search_by_system_name("WOLFRAM_ALPHA_API_KEY")->count() == 0) {
     Setting::Factory()
             ->set_system_name("WOLFRAM_ALPHA_API_KEY")
             ->set_public_name("Wolfram Alpha API Key. By default, this is the 'experimental' key, '8R9245-TEEKKL8X9E'")
