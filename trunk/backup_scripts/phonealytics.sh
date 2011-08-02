4ec11ecf119c9b49c9b37b48f7bcf5f71084817e
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-08-02 15:37:58 +0000

    git-svn-id: http://svn.turbocrms.com/magic@81 031e0832-c032-4354-a283-d89f8bcd3735

--- trunk/backup_scripts/phonealytics.sh
+++ trunk/backup_scripts/phonealytics.sh
@@ -2,7 +2,7 @@
 echo "Preparing backup directory"
 mkdir ~/tmp/backup/phonealytics/ -p 
 cd ~/tmp/backup/phonealytics/
-svn checkout --no-auth-cache --non-interactive --username phonealytics --password 975x2nM4JP5427Q http://svn.turbocrms.com/magicsqlbackups/phonealytics/ .
+svn checkout -q --no-auth-cache --non-interactive --username phonealytics --password 975x2nM4JP5427Q http://svn.turbocrms.com/magicsqlbackups/phonealytics/ .
 
 echo "Exporting database to files..."
 for T in `mysql --host=sql.turbocrms.com --user=phonealytics --password=N7GtUeeyi814g2G -N -B -e 'show tables from phonealytics'`; do 
@@ -11,9 +11,9 @@ for T in `mysql --host=sql.turbocrms.com --user=phonealytics --password=N7GtUeey
 done
 
 echo "Adding tables to subversion...";
-svn add * --non-interactive
+svn add * --non-interactive -q
 echo "Committing...";
-svn commit -m "Committing at `date +%Y/%m/%d\ %H:%M:%S`." --non-interactive --username phonealytics --password 975x2nM4JP5427Q 
+svn commit -q -m "Committing at `date +%Y/%m/%d\ %H:%M:%S`." --non-interactive --username phonealytics --password 975x2nM4JP5427Q 
 cd -
 echo "Removing tmp files...";
 rm ~/tmp/backup/phonealytics/ -Rf
