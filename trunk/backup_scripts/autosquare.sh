e28b17e36b27f4ac34de9be46d06755fff0cc5a2
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-08-02 16:33:38 +0000

    git-svn-id: http://svn.turbocrms.com/magic@84 031e0832-c032-4354-a283-d89f8bcd3735

---
+++ trunk/backup_scripts/autosquare.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Preparing backup directory"
+mkdir ~/tmp/backup/autosquare/ -p 
+cd ~/tmp/backup/autosquare/
+svn checkout -q --no-auth-cache --non-interactive --username autosquare --password e7i30t582VE2MTm http://svn.turbocrms.com/magicsqlbackups/autosquare/ .
+
+echo "Exporting database to files..."
+for T in `mysql --host=sql.turbocrms.com --user=autosquare --password=n75G7ER2oJ5L3LL -N -B -e 'show tables from autosquare'`; do 
+	echo "  > $T.sql"; 
+	mysqldump \
+	--host=sql.turbocrms.com \
+	--user=autosquare \
+	--password=n75G7ER2oJ5L3LL \
+	--skip-comments \
+	--skip-extended-insert \
+	--skip-quick \
+	autosquare \
+	$T > $T.sql;
+done
+
+echo "Adding tables to subversion...";
+svn add * --non-interactive -q
+echo "Committing...";
+svn commit -q -m "Committing at `date +%Y/%m/%d\ %H:%M:%S`." --non-interactive --username autosquare --password e7i30t582VE2MTm 
+cd -
+echo "Removing tmp files...";
+rm ~/tmp/backup/autosquare/ -Rf
+echo "Done!"
