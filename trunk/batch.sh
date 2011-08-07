432bb29107ab9a80bab197360f1301e80cfa3fe0
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-08-07 23:42:03 +0000

    Changes from LIVE
    git-svn-id: http://svn.turbocrms.com/magic@123 031e0832-c032-4354-a283-d89f8bcd3735

--- trunk/batch.sh
+++ trunk/batch.sh
@@ -1,6 +1,4 @@
 for ((i = 0; i <= 500; i++))
 do
-php /home/matthew/Magic/application/bbindex/script/e621.batch.php;
-rsync -rv /home/matthew/Magic/application/bbindex/upload/ ncms@bunnehbutt.com:~/Socializr/application/bbindex/upload/; 
-rm /home/matthew/Magic/application/bbindex/upload/* -Rv
+php /home/bunnehbutt/www/application/BunnehButt/script/e621.batch.php;
 done
