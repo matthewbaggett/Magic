57a8aeb2a9e25c8193276b36587a9daa8f6e0cca
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-09-09 14:30:36 +0000

    git-svn-id: http://svn.turbocrms.com/magic@199 031e0832-c032-4354-a283-d89f8bcd3735

--- trunk/installers/rabbit.sh
+++ trunk/installers/rabbit.sh
@@ -1,3 +1,5 @@
 svn co http://svn.turbocrms.com/magicapps/rabbit/trunk/ ./application/Rabbit
+echo "Checking out plugins/ecommerce..."
+svn co http://svn.turbocrms.com/magicplugins/ecommerce/trunk/ ./plugins/ecommerce
 ./installers/core.sh
 
