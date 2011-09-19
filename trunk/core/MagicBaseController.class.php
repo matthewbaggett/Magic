13fa632d71683337f8db04088829afc9ea6d73b7
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-09-19 12:48:49 +0000

    Various Fixes, including saving edited objects
    git-svn-id: http://svn.turbocrms.com/magic@217 031e0832-c032-4354-a283-d89f8bcd3735

--- trunk/core/MagicBaseController.class.php
+++ trunk/core/MagicBaseController.class.php
@@ -13,7 +13,14 @@ class MagicBaseController
 	
     public function Factory(){
 		throw new exception("Uhoh, there is no Factory() definition in ".get_called_class());
-		
+	}
+	
+	public function is_post(){
+		if(count($_POST) > 0){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
 
