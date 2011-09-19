a15bcb1aefb09d192483b98f70094a9ab2ca8588
geusebio <geusebio@031e0832-c032-4354-a283-d89f8bcd3735>
2011-09-19 17:01:05 +0000

    git-svn-id: http://svn.turbocrms.com/magic@220 031e0832-c032-4354-a283-d89f8bcd3735

--- trunk/core/MagicUtils.class.php
+++ trunk/core/MagicUtils.class.php
@@ -75,9 +75,22 @@ class MagicUtils
 		return true;
 	}
 
+	//
 	static public function widget_select($params, $content, Smarty_Internal_Template $template, &$repeat){
-		return print_r($params);
+		$objectSearcher = $params['object']."Searcher";
+		$arrObject = $objectSearcher::Factory()->sort($params['display'], "ASC")->execute();
+		$options = '';
+		foreach($arrObject as $oObject){
+			$value = call_user_method("get_{$params['value']}", $oObject);
+			$label = call_user_method("get_{$params['display']}", $oObject);
+			$selected = $params['selected'] == $value?'selected="selected" ':'';
+			$options.= "<option value=\"{$value}\" {$selected}>";
+			$options.= $label;
+			$options.= "</option>\n";
+		}
+		return "<select name=\"{$params['name']}\" id=\"{$params['name']}\">\n{$options}\n</select>\n";
 	}
+	
 	/**
 	 * Display fuzzy time. IE: "One week ago"
 	 */
